f95a28dc43efc63f06b397fde37b2e369ec0e5a8
Deep <deepsingh.cts@gmail.com>
2019-12-23 03:15:30 +0530

    built graphql api for create and get events

--- app.js
+++ app.js
@@ -6,18 +6,34 @@ const { buildSchema } = require('graphql');
 const port = 3000;
 const app = express();
 
+const events = []
+
 console.log('hello graphql');
 
 app.use(bodyParser.json());
 
 app.use('/graphql', graphqlHttp({
     schema: buildSchema(`
+        type Event {
+            _id: ID
+            title: String!
+            description: String!
+            price: Float!
+            date: String!
+        }
+        input EventInput {
+            title: String!
+            description: String!
+            price: Float!
+            date: String!
+        }
+
         type RootQuery {
-            events: [String!]!
+            events: [Event!]!
         }
 
         type RootMutation {
-            createEvent(name: String): String
+            createEvent(eventInput: EventInput): Event
         }
 
         schema { 
@@ -25,13 +41,21 @@ app.use('/graphql', graphqlHttp({
             mutation: RootMutation
         }
     `),
-    rootValue: { //resolvers as per types
+    rootValue: { //resolvers: naming as per types
         events: () => {
-            return ['Romantic Cooking', 'Dancing', 'Coding'];
+            return events;
         },
         createEvent: (args) => {
-            const eventName = args.name;
-            return eventName;
+            console.log('args: ', args.eventInput);
+            const event  = {
+                _id: Math.random().toString(),
+                title: args.eventInput.title,
+                description: args.eventInput.description,
+                price: +args.eventInput.price,
+                date: new Date().toISOString()
+            }
+            events.push(event)
+            return event
         }
     },
     graphiql: true
